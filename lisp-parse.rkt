114e6d9d466b08f4174e2097d8ac2e57f5d30c37
Joe Ardent <code@ardent.nebcorp.com>
2016-01-01 20:41:41 -0800

    definitely works

--- lisp-parse.rkt
+++ lisp-parse.rkt
@@ -2,15 +2,15 @@
 
 (provide parse-lisp)
 
-(define reserved '(first rest car cdr list cons lambda cond if quote))
-
 (define (parse-lisp str)
   str)
 
 (define (tokenize str)
-  (string-split (string-trim (string-replace
-                               (string-replace str "(" " ( ")
-                               ")" " ) "))))
+  (string-split
+   (string-trim
+    (let* ([noqm (string-replace str "'(" "(quote ")]
+           [spaced-open (string-replace noqm "(" " ( ")])
+      (string-replace spaced-open ")" " ) ")))))
 
 (define (string-first s)
   (string-ref s 0))
@@ -18,16 +18,21 @@
 (define (string-last s)
   (string-ref s (sub1 (string-length s))))
 
+(define (pstring? s)
+  (let ([s0 (string-first s)]
+        [s1 (string-last s)])
+    (and (eqv? #\" s0) (eqv? s0 s1))))
+
 (define (classify tok)
-  tok)
+  (let ([num (string->number tok)])
+    (cond
+     [(or num (pstring? tok)) (cons 'literal tok)]
+     [else (cons 'identifier tok)])))
 
 (define (ensure-list l)
   (if (list? l)
       l
       (list l)))
-
-(define (atom? e)
-  (not (list? e)))
 
 (define (p-append head tail)
   (let ([lhead (ensure-list head)])
@@ -36,22 +41,31 @@
      [(null? tail) lhead]
      [else (append lhead (list tail))])))
 
+(define (wrap-subterm sterm level)
+  (cond
+   [(< level 1) sterm]
+   [(null? sterm) sterm]
+   [else
+    (for/fold ([out sterm])
+        ([l level])
+      (list out))]))
+
 (define (parenthesize toks)
-  (define (p-help toks out)
-    (displayln out)
+  (define (p-help toks out level)
     (cond
-     ;[(atom? toks) (p-append out toks)]
      [(null? toks) out]
      [else
       (let* ([tok (car toks)]
              [rest (cdr toks)])
         (match tok
-          ["(" (append (ensure-list out) (p-help rest '()))]
-          [")" (append (list out) (p-help rest '()))]
-          [_ (p-help rest (p-append out (classify tok)))]))]))
-  (p-help toks '()))
+          ["(" (append (ensure-list out) (p-help rest '() (add1 level)))]
+          [")" (if (not (null? out))
+                   (append (wrap-subterm (ensure-list out) level)
+                           (p-help rest '() (sub1 level)))
+                   (append out (p-help rest '() (sub1 level))))]
+          [_ (p-help rest (p-append out (classify tok)) level)]))]))
+  (p-help toks '() -1))
 
 (begin
-  (define toks (tokenize "(a b c d (1 2 3 4) a1 (b1 c1) d1)"))
-  (define test (parenthesize toks))
-  (displayln test))
+  (define toks (tokenize "(a b c d (((1 2 3 4))) a1 a2 (b1 c1) d1 ((e1)) e2 e3 (e4 e5)))"))
+  (define test (parenthesize toks)))
